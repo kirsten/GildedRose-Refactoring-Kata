c26dfa8c9a7dc0dd42da34a692d8a7edd2b5c8c7
tylerjhh <tyler.jh.herrmann@gmail.com>
2020-06-24 20:26:05 -0400

    Add a test for item name

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,9 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+  it("has the correct name", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("Aged Brie");
   });
 });

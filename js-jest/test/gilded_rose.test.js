0a9561b909391cc0a5087542112b466a22b521ad
Kirsten Nordine <kirstennordine@gmail.com>
2020-06-28 19:03:56 -0400

    Add unit test coverage for standard items

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,58 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
+// Special items:
+//
+// 'Aged Brie'
+// 'Backstage passes to a TAFKAL80ETC concert'
+// 'Sulfuras, Hand of Ragnaros'
+
 describe("Gilded Rose", function() {
   it("has the correct name", function() {
     const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("Aged Brie");
+  });
+
+  describe("updateQuality", function() {
+    describe("standard items", function() {
+      describe("when the sell by date has not yet passed", function() {
+        it("quality degrades by 1 every day", function() {
+          const gildedRose = new Shop([new Item("+5 Dexterity Vest", 9, 19)]);
+
+          const items = gildedRose.updateQuality();
+
+          expect(items[0].quality).toBe(18);
+        });
+
+        it("quality does not become negative", function() {
+          const gildedRose = new Shop([new Item("+5 Dexterity Vest", 0, 1)]);
+
+          gildedRose.updateQuality();
+          const items = gildedRose.updateQuality();
+
+          expect(items[0].quality).toBe(0);
+        });
+      });
+
+      describe("on the sell by date", function() {
+        it("quality degrades twice as fast", function() {
+          const gildedRose = new Shop([new Item("+5 Dexterity Vest", 1, 19)]);
+
+          const items = gildedRose.updateQuality();
+
+          expect(items[0].quality).toBe(18);
+        });
+      });
+
+      describe("when the sell by date has passed", function() {
+        it("quality degrades twice as fast", function() {
+          const gildedRose = new Shop([new Item("+5 Dexterity Vest", 0, 19)]);
+
+          const items = gildedRose.updateQuality();
+
+          expect(items[0].quality).toBe(17);
+        });
+      });
+    });
   });
 });

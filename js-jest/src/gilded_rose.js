8a9279e8ac064f8ed01bb83776e1650ea487b502
Kirsten Nordine <kirstennordine@gmail.com>
2020-07-08 19:47:35 -0400

    extract variable for maxQuality

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -11,6 +11,8 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
+    const maxQuality = 50;
+
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
@@ -19,16 +21,16 @@ class Shop {
           }
         }
       } else {
-        if (this.items[i].quality < 50) {
+        if (this.items[i].quality < maxQuality) {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
+              if (this.items[i].quality < maxQuality) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
             if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
+              if (this.items[i].quality < maxQuality) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
@@ -50,7 +52,7 @@ class Shop {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
-          if (this.items[i].quality < 50) {
+          if (this.items[i].quality < maxQuality) {
             this.items[i].quality = this.items[i].quality + 1;
           }
         }

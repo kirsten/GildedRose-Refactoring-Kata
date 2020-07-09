90db3609e7b16bbb8673d85cf6d2856c2e968e1d
tylerjhh <tyler.jh.herrmann@gmail.com>
2020-07-08 20:23:06 -0400

    Move repeated conditional to function

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -20,16 +20,16 @@ class Shop {
   }
 
   decrementItemQuality(item) {
-    item.quality = item.quality - 1;
+    if (item.quality > 0) {
+      item.quality = item.quality - 1;
+    }
   }
 
   updateQuality() {
     this.items.forEach(item => {
       if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            this.decrementItemQuality(item);
-          }
+        if (item.name != 'Sulfuras, Hand of Ragnaros') {
+          this.decrementItemQuality(item);
         }
       } else {
           this.incrementItemQuality(item)
@@ -48,10 +48,8 @@ class Shop {
       if (item.sellIn < 0) {
         if (item.name != 'Aged Brie') {
           if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                this.decrementItemQuality(item);
-              }
+            if (item.name != 'Sulfuras, Hand of Ragnaros') {
+              this.decrementItemQuality(item);
             }
           } else {
             item.quality = item.quality - item.quality;

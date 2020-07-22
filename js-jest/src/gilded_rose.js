17f23cbe3eba894d3034a6b9f08d9b2273b0695a
Madelyn Freed <madelyn.freed@gmail.com>
2020-07-22 19:59:14 -0400

    Extract hand of ragnaros

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -29,7 +29,6 @@ class Shop {
     this.items.forEach(item => {
       if (item.name === 'Sulfuras, Hand of Ragnaros') {
         // doesn't age & might increase in quality
-
       } else {
         if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
           this.decrementItemQuality(item);
@@ -49,9 +48,7 @@ class Shop {
         if (item.sellIn < 0) {
           if (item.name != 'Aged Brie') {
             if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
                 this.decrementItemQuality(item);
-              }
             } else {
               item.quality = item.quality - item.quality;
             }

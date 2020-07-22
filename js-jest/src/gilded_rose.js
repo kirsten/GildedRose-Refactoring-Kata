48a11782aff754dd2b7dfb476d79811722534b6a
Sungwon Ma <sungwon.ma.16@gmail.com>
2020-07-22 19:53:55 -0400

    wip

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -27,37 +27,40 @@ class Shop {
 
   updateQuality() {
     this.items.forEach(item => {
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.name != 'Sulfuras, Hand of Ragnaros') {
-          this.decrementItemQuality(item);
-        }
+      if (item.name === 'Sulfuras, Hand of Ragnaros') {
+        // doesn't age & might increase in quality
+
       } else {
+        if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+          this.decrementItemQuality(item);
+        } else {
           this.incrementItemQuality(item)
           if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.sellIn < 11) {
-                this.incrementItemQuality(item)
+              this.incrementItemQuality(item)
             }
             if (item.sellIn < 6) {
-                this.incrementItemQuality(item)
+              this.incrementItemQuality(item)
             }
           }
-      }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        }
         item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-              this.decrementItemQuality(item);
+
+        if (item.sellIn < 0) {
+          if (item.name != 'Aged Brie') {
+            if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+              if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                this.decrementItemQuality(item);
+              }
+            } else {
+              item.quality = item.quality - item.quality;
             }
           } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
             this.incrementItemQuality(item)
+          }
         }
       }
+
     });
 
     return this.items;

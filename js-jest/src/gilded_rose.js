a99e9b109813771bc933e13d588c4fb385146d56
India Amos <india.amos@blackboardinsurance.com>
2020-07-22 20:28:55 -0400

    Move aged brie stuff into a new if branch

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -29,7 +29,7 @@ class Shop {
     this.items.forEach(item => {
       if (item.name === 'Sulfuras, Hand of Ragnaros') {
         // doesn't age & might increase in quality
-      } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
         this.incrementItemQuality(item)
         if (item.sellIn < 11) {
           this.incrementItemQuality(item)
@@ -41,20 +41,18 @@ class Shop {
         if (item.sellIn < 0) {
           item.quality = item.quality - item.quality;
         }
-      } else {
-        if (item.name != 'Aged Brie') {
-          this.decrementItemQuality(item);
-        } else {
+      } else if (item.name === 'Aged Brie') {
+        this.incrementItemQuality(item);
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
           this.incrementItemQuality(item)
         }
+      } else {
+        this.decrementItemQuality(item);
         item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
-          if (item.name != 'Aged Brie') {
-            this.decrementItemQuality(item);
-          } else {
-            this.incrementItemQuality(item)
-          }
+          this.decrementItemQuality(item);
         }
       }
     });

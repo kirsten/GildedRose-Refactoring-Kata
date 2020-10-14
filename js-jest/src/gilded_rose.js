c1d4ebdbdcdd67e3780bd55d9522170f38707403
tylerjhh <tyler.jh.herrmann@gmail.com>
2020-10-14 19:59:28 -0400

    Remove item arguments from Item methods

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -17,17 +17,17 @@ class Item {
     this.quality = this.quality + amount;
   }
 
-  updateExpiredItem(item, amount) {
-    if (item.sellIn < 0) {
+  updateExpiredItem(amount) {
+    if (this.sellIn < 0) {
       this.changeQuality(amount);
     }
   }
 
-  changeItemQualityBySellinDate(item) {
-    if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
-      if (item.sellIn < 6) {
+  changeItemQualityBySellinDate() {
+    if (this.name === "Backstage passes to a TAFKAL80ETC concert") {
+      if (this.sellIn < 6) {
         this.changeQuality(3);
-      } else if (item.sellIn < 11) {
+      } else if (this.sellIn < 11) {
         this.changeQuality(2);
       } else {
         this.changeQuality(1);
@@ -35,31 +35,31 @@ class Item {
     }
   }
 
-  updateSellinDate(item, amount) {
-    item.sellIn = item.sellIn - amount;
+  updateSellinDate(amount) {
+    this.sellIn = this.sellIn - amount;
   }
 
-  updateItemQuality(item) {
-    if (item.name === "Sulfuras, Hand of Ragnaros") {
-      this.changeItemQualityBySellinDate(item);
-      this.updateSellinDate(item, 0);
+  updateItemQuality() {
+    if (this.name === "Sulfuras, Hand of Ragnaros") {
+      this.changeItemQualityBySellinDate();
+      this.updateSellinDate(0);
       this.changeQuality(0);
-      this.updateExpiredItem(item, 0);
-    } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
-      this.changeItemQualityBySellinDate(item);
-      this.updateSellinDate(item, 1);
+      this.updateExpiredItem(0);
+    } else if (this.name === "Backstage passes to a TAFKAL80ETC concert") {
+      this.changeItemQualityBySellinDate();
+      this.updateSellinDate(1);
       this.changeQuality(0);
-      this.updateExpiredItem(item, -item.quality);
-    } else if (item.name === "Aged Brie") {
-      this.changeItemQualityBySellinDate(item);
-      this.updateSellinDate(item, 1);
+      this.updateExpiredItem(-this.quality);
+    } else if (this.name === "Aged Brie") {
+      this.changeItemQualityBySellinDate();
+      this.updateSellinDate(1);
       this.changeQuality(1);
-      this.updateExpiredItem(item, 1);
+      this.updateExpiredItem(1);
     } else {
-      this.changeItemQualityBySellinDate(item);
-      this.updateSellinDate(item, 1);
+      this.changeItemQualityBySellinDate();
+      this.updateSellinDate(1);
       this.changeQuality(-1);
-      this.updateExpiredItem(item, -1);
+      this.updateExpiredItem(-1);
     }
   }
 }
@@ -71,7 +71,7 @@ class Shop {
 
   updateQuality() {
     this.items.forEach((item) => {
-      item.updateItemQuality(item);
+      item.updateItemQuality();
     });
   }
 }
